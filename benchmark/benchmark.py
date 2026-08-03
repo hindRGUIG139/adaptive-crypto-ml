e3dec48df224568d4d9dd8ea1f9e308fb1177591
Hind <hindrguig139@gmail.com>
2026-08-03 18:53:42 +0100

    update the benchmark code

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -5,12 +5,14 @@ from crypto_algorithms.aes import encrypt_file as aes_encrypt
 from crypto_algorithms.present import encrypt_file as present_encrypt
 from crypto_algorithms.chacha20 import encrypt_file as chacha20_encrypt
 import sys
+from monitoring.system_metrics import get_battery_level, get_cpu_usage
+from monitoring.file_info import get_file_info
+import csv
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.insert(0, PROJECT_ROOT)
 def benchmark_algorithm(encrypt_function, input_file, output_file, key):
-
     times = []
     for _ in range(10):
         start = time.perf_counter()
@@ -21,30 +23,63 @@ def benchmark_algorithm(encrypt_function, input_file, output_file, key):
     average_time = statistics.mean(times)
     standard_deviation = statistics.stdev(times)
 
-    return average_time, standard_deviation
+    file_info = get_file_info(input_file)
+    file_size = file_info["size_bytes"]
+    file_size_mb = file_size / (1024 * 1024)
+    throughput = file_size_mb / average_time
+    cpu = get_cpu_usage()
+    battery = get_battery_level()
+    return average_time, standard_deviation, cpu, battery, throughput
+
+def save_to_csv(data_type, file_size, algorithm, average_time, standard_deviation, throughput, cpu, battery):
+    with open("ml/dataset.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        if file.tell() == 0:
+            writer.writerow(["Data Type","File Size (Bytes)", "Algorithm", "Average Time (s)", "Standard Deviation (s)",
+                            "CPU Usage (%)","Battery Level (%)" ])
+        writer.writerow([data_type, file_size, algorithm, average_time, standard_deviation, throughput, cpu, battery])
+
 
 def main():
-    input_file = "data/sample_100KB.txt"
-    output_file = "data/sample_100KB.enc"
+    input_file = "data/vtest.mp4"
+    # file_info
+    file_info = get_file_info(input_file)
+    data_type = file_info["data_type"]
+    file_size = file_info["size_bytes"]
+    output_file = "data/vtest.enc"
+    # keys
     key_aes = os.urandom(16)  # 128-bit key for AES
     key_present = int.from_bytes(os.urandom(10), "big")  # 80-bit key for PRESENT
     key_chacha20= os.urandom(32)  # 256-bit key for ChaCha20
     print("Benchmarking AES...")
-    aes_time, aes_std = benchmark_algorithm(aes_encrypt, input_file, output_file, key_aes)
+    aes_time, aes_std, aes_cpu, aes_battery, aes_throughput = benchmark_algorithm(aes_encrypt, input_file, output_file, key_aes)
 
     print("Benchmarking PRESENT...")
-    present_time, present_std = benchmark_algorithm(present_encrypt, input_file, output_file, key_present)
-    print("Benchmarking ChaCha20...")
-    chacha20_time, chacha20_std = benchmark_algorithm(chacha20_encrypt, input_file, output_file, key_chacha20)
+    present_time, present_std ,present_cpu, present_battery, present_throughput = benchmark_algorithm(present_encrypt, input_file, output_file, key_present)
 
+    print("Benchmarking ChaCha20...")
+    chacha20_time, chacha20_std ,chacha20_cpu, chacha20_battery, chacha20_throughput= benchmark_algorithm(chacha20_encrypt, input_file, output_file, key_chacha20)
+    #aes
     print(f"AES average encryption time: {aes_time:.6f} seconds")
     print(f"AES standard deviation: {aes_std:.6f} seconds")
-
+    print(f"AES throughput: {aes_throughput:.2f} MB/s")
+    print(f"CPU Usage: {aes_cpu}%")
+    print(f"Battery Level: {aes_battery}%")
+    save_to_csv(data_type, file_size, "AES", aes_time, aes_std, aes_throughput, aes_cpu, aes_battery)
+    #present
     print(f"PRESENT average encryption time: {present_time:.6f} seconds")
     print(f"PRESENT standard deviation: {present_std:.6f} seconds")
-
+    print(f"PRESENT throughput: {present_throughput:.2f} MB/s")
+    print(f"CPU Usage: {present_cpu}%")
+    print(f"Battery Level: {present_battery}%")
+    save_to_csv(data_type, file_size, "PRESENT", present_time, present_std, present_throughput, present_cpu, present_battery)
+    #chacha20
     print(f"ChaCha20 average encryption time: {chacha20_time:.6f} seconds")
     print(f"ChaCha20 standard deviation: {chacha20_std:.6f} seconds")
+    print(f"ChaCha20 throughput: {chacha20_throughput:.2f} MB/s")
+    print(f"CPU Usage: {chacha20_cpu}%")
+    print(f"Battery Level: {chacha20_battery}%")
+    save_to_csv(data_type, file_size, "ChaCha20", chacha20_time, chacha20_std, chacha20_throughput, chacha20_cpu, chacha20_battery)
 
 if __name__ == "__main__":
     main()
