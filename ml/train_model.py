9038b208a6a8e44238d8ce517f72641bc4eb19eb
Hind <hindrguig139@gmail.com>
2026-08-21 13:26:55 +0100

    update the training code

--- ml/train_model.py
+++ ml/train_model.py
@@ -4,11 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+import matplotlib.pyplot as plt
 import xgboost as xgb
 df= pd.read_csv('ml/training_dataset.csv')
 
-X= df[["Data Type", "File Size (Bytes)", "CPU Usage (%)", "Battery Level (%)"]]
-y=df["Algorithms"]
+X = df[["Data Type", "File Size (Bytes)", "CPU Ambient (%)", "Battery Ambient (%)"]].copy()
+y = df["Best Algorithm"]
 
 data_type_encoder=LabelEncoder()
 Algorithms_encoder=LabelEncoder()
@@ -17,11 +20,21 @@ X["Data Type"] = data_type_encoder.fit_transform(X["Data Type"])
 y = Algorithms_encoder.fit_transform(y)
 
 # the last parameter fixes the random seed so the results are the same every time the program runs.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 rf_model = RandomForestClassifier(
     n_estimators=100, # Build a Random Forest using 100 decision trees.
     random_state=42
 )
+
+
+
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+scores = cross_val_score(rf_model, X, y, cv=cv, scoring="accuracy")
+
+print("Cross-validation scores:", scores)
+print(f"Mean accuracy: {scores.mean():.2f}")
+print(f"Standard deviation: {scores.std():.2f}")
 # # Train the Random Forest using the training data.
 rf_model.fit(X_train, y_train) 
 # Make predictions on the test data.
@@ -32,7 +45,7 @@ print(f"Random Forest Accuracy: {rf_accuracy:.2f}")
 print(classification_report(y_test, rf_predictions)) #A detailed report card.
 
 # xgboost model
-xgb_model = xgb.XGBClassifier( random_state=42, eval_metrics = "mlogloss")
+xgb_model = xgb.XGBClassifier( random_state=42, eval_metric = "mlogloss")
 # train the model using the training data.
 xgb_model.fit(X_train, y_train)
 # Make predictions on the test data.
@@ -50,3 +63,12 @@ if rf_accuracy >= xgb_accuracy:
     joblib.dump(rf_model, "ml/best_model.pkl")
 else:
     joblib.dump(xgb_model, "ml/best_model.pkl")
+
+cm = confusion_matrix(y_test, xgb_predictions)
+disp = ConfusionMatrixDisplay(cm, display_labels=Algorithms_encoder.classes_)
+disp.plot()
+plt.show(block=False)
+plt.pause(2)
+plt.close()
+
+print(df["Best Algorithm"].value_counts())
